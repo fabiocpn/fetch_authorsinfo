4812053d8871b8087c404c4ccee4518abdc57397
Fabio Navarro <fabiocpn@gmail.com>
2017-12-16 00:07:01 +0000

    First commit
    Playing around with a script to fetch papers from a given author
    or list of authors (using ";"), count the number of papers and
    extract the keywords from his abstracts; a lot of space for
    improvements.

---
+++ fetch_pubmed.py
@@ -0,0 +1,76 @@
+import argparse
+from Bio import Entrez
+from Bio import Medline
+import RAKE
+
+MAX_COUNT = 500
+
+def read_args():
+    '''
+    read arguments for script
+    '''
+
+    global author_name
+    parser = argparse.ArgumentParser(description='task automation ' +
+                                     'with BAM')
+    parser.add_argument('--author-name',
+                        help='author name to be searched',
+                        required=True)
+    args = parser.parse_args()
+    author_name = args.author_name
+    return
+
+def dummy_function(params):
+    '''
+    Find all files with an extension within a project
+    @args api: api instance with user logged in
+    @args project: project instance
+    @args extension: extension for finding files
+    @return my_files: all files with a particular extension
+    '''
+
+    return params
+
+
+def main(author_name):
+    # Initialize api instance
+    authors = author_name.split(";")
+    for name in authors:
+        TERM = name+"[AU]"
+    
+        #print('Getting {0} publications containing {1}...'.format(MAX_COUNT, TERM))
+        Entrez.email = 'fabiocpn@gmail.com'
+        h = Entrez.esearch(db='pubmed', retmax=MAX_COUNT, term=TERM)
+        result = Entrez.read(h)
+
+        n_publications = result['Count']
+        ids = result['IdList']
+        h = Entrez.efetch(db='pubmed', id=ids, rettype='medline', retmode='text')
+        records = Medline.parse(h)
+    
+        titles = []
+        for record in records:
+            abstract = record.get('AB', '?')
+            titles.append(abstract)
+        #print('Titles: {0}'.format('; '.join(titles)))
+        #r = Rake("SmartStoplist.txt", 3, 3, 2) 
+        Rake = RAKE.Rake(RAKE.SmartStopList())
+            #r = Rake(<language>)
+        #keywords = Rake.run(". ".join(titles),minCharacters = 1, maxWords = 3, minFrequency = 3) 
+        keywords = Rake.run(". ".join(titles),minCharacters = 1, maxWords = 2, minFrequency = 3) 
+        keywords_list = []
+        count_keywords = 0
+        for key in keywords:
+            keywords_list.append(key[0])
+            count_keywords = count_keywords + 1
+            if ( count_keywords >= 12 ):
+                break
+    
+        print name+" "+n_publications+" \""+", ".join(keywords_list)+"\""
+    
+    return
+
+if __name__ == "__main__":
+    read_args()
+    main(author_name)
+
