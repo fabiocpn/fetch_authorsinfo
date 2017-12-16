c42b17765605d80f4a6dbfd13b50cbc7e3d087c5
Fabio Navarro <fabiocpn@gmail.com>
2017-12-16 02:53:14 +0000

    Parameterize search fiels
    Neat! Now I can search for extra fields through the
    command line:
     --additional-fields "AD:Miami"
     --additional-fields "AD:Miami;AU:Navarro" for example

--- fetch_pubmed.py
+++ fetch_pubmed.py
@@ -10,14 +10,18 @@ def read_args():
     read arguments for script
     '''
 
-    global author_name
+    global author_name,additional_fields
     parser = argparse.ArgumentParser(description='task automation ' +
                                      'with BAM')
     parser.add_argument('--author-name',
                         help='author name to be searched',
                         required=True)
+    parser.add_argument('--additional-fields',
+            help='extra pubmed fields and serches. FORMAT=FIELD:STRING',
+                        required=False)
     args = parser.parse_args()
     author_name = args.author_name
+    additional_fields = args.additional_fields
     return
 
 def dummy_function(params):
@@ -32,9 +36,14 @@ def dummy_function(params):
     return params
 
 
-def main(author_name):
+def main(author_name,additional_fields):
     # Initialize api instance
     authors = author_name.split(";")
+    if ( additional_fields ):
+        search_tuples = additional_fields.split(";")
+    else:
+        search_tuples = []
+
     for name in authors:
         TERM = name+"[AU]"
     
@@ -48,21 +57,30 @@ def main(author_name):
         h = Entrez.efetch(db='pubmed', id=ids, rettype='medline', retmode='text')
         records = Medline.parse(h)
     
-        titles = []
+        abstracts = []
         NSC = 0
+        additional_fields_count = [0] * len(search_tuples)
         for record in records:
             abstract = record.get('AB', '?')
-            titles.append(abstract)
+            abstracts.append(abstract)
             journal = record.get('TA','?')
             if ( journal == "Nature" or journal == "Science" or journal == "Cell" ):
                 NSC = NSC + 1
-            print record
+ 
+            field_index=0
+            for search_tuple in search_tuples: 
+                search_tokens = search_tuple.split(":")
+                field_value = record.get(search_tokens[0],'?')
+#                print "==>"+ search_tokens[1] + " " + field_value
+                if ( field_value.find(search_tokens[1]) != -1):
+                   additional_fields_count[field_index]=additional_fields_count[field_index] + 1
+                field_index = field_index + 1
 
         #r = Rake("SmartStoplist.txt", 3, 3, 2) 
         Rake = RAKE.Rake(RAKE.SmartStopList())
             #r = Rake(<language>)
         #keywords = Rake.run(". ".join(titles),minCharacters = 1, maxWords = 3, minFrequency = 3) 
-        keywords = Rake.run(". ".join(titles),minCharacters = 1, maxWords = 2, minFrequency = 3) 
+        keywords = Rake.run(". ".join(abstracts),minCharacters = 1, maxWords = 2, minFrequency = 3) 
         keywords_list = []
         count_keywords = 0
         for key in keywords:
@@ -71,11 +89,11 @@ def main(author_name):
             if ( count_keywords >= 12 ):
                 break
     
-        #print name+"\t"+n_publications+"\t"+str(NSC)+"\t"+"\""+", ".join(keywords_list)+"\""
+        print name+"\t"+n_publications+"\t"+str(NSC)+"\t"+"\t".join(str(x) for x in additional_fields_count)+"\t\""+", ".join(keywords_list)+"\""
     
     return
 
 if __name__ == "__main__":
     read_args()
-    main(author_name)
+    main(author_name,additional_fields)
 
